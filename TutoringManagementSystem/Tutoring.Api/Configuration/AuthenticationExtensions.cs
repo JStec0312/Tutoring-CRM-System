bfba926b49f852bf4c9d32a58595b2160e0987d9
Jakub Stec <stecu03@gmail.com>
2026-08-19 21:23:57 +0200

    RE #38 Added config for JWT bearer

--- TutoringManagementSystem/Tutoring.Api/Configuration/AuthenticationExtensions.cs
+++ TutoringManagementSystem/Tutoring.Api/Configuration/AuthenticationExtensions.cs
@@ -1,5 +1,8 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Tutoring.Infrastructure.Authentication;
 
 namespace Tutoring.Api.Configuration;
@@ -11,8 +14,44 @@ public static class AuthenticationExtensions
         this IServiceCollection services,
         IConfiguration configuration)
     {
+        // JWT options (key and expiration) are configured in appsettings.json
         services.Configure<JwtOptions>(
             configuration.GetSection(JwtOptions.SectionName));
+
+        var jwtOptions = configuration.GetSection(JwtOptions.SectionName).Get<JwtOptions>();
+        // Bearer Options
+        services
+            .AddAuthentication(
+                JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.MapInboundClaims = false;
+
+                options.TokenValidationParameters =
+                    new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtOptions.Issuer,
+
+                        ValidateAudience = true,
+                        ValidAudience = jwtOptions.Audience,
+
+                        ValidateLifetime = true,
+
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(
+                                Encoding.UTF8.GetBytes(
+                                    jwtOptions.SecretKey)),
+
+                        RoleClaimType = "role",
+                        NameClaimType = "sub",
+
+                        ClockSkew = TimeSpan.Zero
+                    };
+            });
+
+        services.AddAuthorization();
 
         return services;
     }
